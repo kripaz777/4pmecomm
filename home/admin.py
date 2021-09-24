8cf9cde0bd341e4a96e3b171bb35eb20e984fb1e
kripaz777 <aceraayush@gmail.com>
2021-09-24 16:52:59 +0545

    Models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Subcategory)
+admin.site.register(Item)
+admin.site.register(Ad)
+admin.site.register(Slider)
+admin.site.register(Contact)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,63 @@
 from django.db import models
-
+STATUS = (('active','active'),('','default'))
+LABELS = (('new','new'),('hot','hot'),('sale','sale'),('','default'))
+STOCK = (('In Stock','In Stock'),('Out of Stock','Out of Stock'))
 # Create your models here.
+class Category(models.Model):
+	title = models.CharField(max_length = 400)
+	slug = models.CharField(max_length= 500,unique = True)
+	description = models.TextField(blank = True)
+	image = models.ImageField(upload_to = 'media')
+	def __str__(self):
+		return self.title
+
+class Subcategory(models.Model):
+	title = models.CharField(max_length = 400)
+	category = models.ForeignKey(Category,models.CASCADE)
+	slug = models.CharField(max_length= 500,unique = True)
+	description = models.TextField(blank = True)
+	image = models.ImageField(upload_to = 'media')
+	def __str__(self):
+		return self.title
+
+class Item(models.Model):
+	title = models.CharField(max_length = 400)
+	slug = models.CharField(max_length = 500,unique = True)
+	price = models.IntegerField()
+	discounted_price = models.IntegerField(default = 0)
+	description = models.TextField(blank = True)
+	image = models.ImageField(upload_to = 'media')
+	category = models.ForeignKey(Category,models.CASCADE)
+	subcategory = models.ForeignKey(Subcategory,models.CASCADE)
+	stock = models.CharField(choices = STOCK,max_length = 50)
+	labels = models.CharField(choices = LABELS,max_length = 50)
+
+	def __str__(self):
+		return self.title
+
+class Ad(models.Model):
+	title = models.CharField(max_length=500)
+	description = models.TextField(blank = True)
+	image = models.ImageField(upload_to = 'media')
+	rank = models.IntegerField(default = 1)
+
+	def __str__(self):
+		return self.title
+
+class Slider(models.Model):
+	title = models.CharField(max_length=500)
+	description = models.TextField(blank = True)
+	image = models.ImageField(upload_to = 'media')
+	rank = models.IntegerField(default = 1)
+	status = models.CharField(choices = STATUS,blank = True,max_length = 50)
+
+	def __str__(self):
+		return self.title
+
+class Contact(models.Model):
+	name = models.CharField(max_length = 500)
+	email = models.EmailField(max_length = 500)
+	message = models.TextField()
+
+	def __str__(self):
+		return self.title
