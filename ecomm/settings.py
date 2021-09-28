a0b47803c12cf32971ed734bb08507cb29d8bff0
kripaz777 <aceraayush@gmail.com>
2021-09-28 17:18:01 +0545

    home page

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ecomm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from .models import *
 # Create your views here.
+class BaseView(View):
+	views = {}
+
+
+class HomeView(BaseView):
+	def get(self,request):
+		self.views['items'] = Item.objects.all()
+		self.views['categories'] = Category.objects.all()
+		self.views['sliders'] = Slider.objects.all()
+
+		return render(request,'index.html')
